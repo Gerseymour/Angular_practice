c4e1529a63e64286e6e1cdd01075f57998acb46c
gerseymour <george.anthony.seymour@gmail.com>
2021-05-19 19:22:54 +0100

    level 6 start

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  {id:1 , name: 'Batman'},
+  {id:2 , name: 'Martian Manhunter'},
+  {id:3, name: 'Superman'},
+  {id:4 , name: 'Green Arrow'},
+  {id:5 , name: 'Black Canery'},
+  {id:6 , name: 'Wonder Woman'},
+  {id:7 , name: 'Green Lantern'},
+  {id:8 , name: 'Atom'},
+  {id:9 , name: 'Aquaman'},
+  {id:10 , name: 'Flash'},
+]
